e7cc0792ecb0af722250db751e2485a09860324f
Keita <watanabe@liquidinc.asia>
2017-12-16 17:59:51 +0900

    プロジェクトの作成とメインメニューの作成

---
+++ XPPassbook/XPPassbook/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace XPPassbook
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public ICommand ExitCommand { get; private set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel()
+        {
+            ExitCommand = new DelegateCommand(() => Application.Current.MainWindow.Close() );
+        }
+    }
+}
