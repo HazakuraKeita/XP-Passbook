fb4306b783a87941e37eab2b78339a0af7498865
Keita <watanabe@liquidinc.asia>
2017-12-16 20:14:23 +0900

    プロパティをModelに移動

---
+++ XPPassbook/XPPassbook.Models/XPer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace XPPassbook.Models
+{
+    public class XPer
+    {
+        public int InvestmentAmount { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public BitmapImage IconImage { get; set; }
+        public string ImageFullName
+        {
+            get { return imageFullName; }
+            set
+            {
+                imageFullName = value;
+
+                if (!string.IsNullOrEmpty(imageFullName))
+                {
+                    var image = new BitmapImage();
+                    image.CreateOptions = BitmapCreateOptions.None;
+                    image.BeginInit();
+                    image.UriSource = new Uri(imageFullName, UriKind.Absolute);
+                    image.EndInit();
+                    IconImage = image;
+                }
+            }
+        }
+
+        string imageFullName;
+        SettingsXml xml;
+
+        public XPer()
+        {
+            xml = new SettingsXml();
+
+            if (xml.IsSuccess)
+            {
+                InvestmentAmount = xml.InvestmentAmount;
+                Name = xml.Name;
+                Address = xml.Address;
+                ImageFullName = xml.ImageFullName;
+            }
+        }
+
+        public void Update()
+        {
+            xml.InvestmentAmount = InvestmentAmount;
+            xml.Name = Name;
+            xml.Address = Address;
+            xml.ImageFullName = ImageFullName;
+            xml.Update();
+        }
+    }
+}
--- XPPassbook/XPPassbook/ViewModel.cs
+++ XPPassbook/XPPassbook/ViewModel.cs
@@ -15,95 +15,25 @@ namespace XPPassbook
 {
     public class ViewModel : INotifyPropertyChanged
     {
-        public int InvestmentAmount
-        {
-            get { return investmentAmount; }
-            set
-            {
-                investmentAmount = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(InvestmentAmount)));
-            }
-        }
-        public string Name
-        {
-            get { return name; }
-            set
-            {
-                name = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
-            }
-        }
-        public string Address
-        {
-            get { return address; }
-            set
-            {
-                address = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Address)));
-            }
-        }
-        public BitmapImage IconImage
-        {
-            get { return iconImage; }
-            set
-            {
-                iconImage = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IconImage)));
-            }
-        }
+        public XPer XPer { get; set; }
         public ICommand ImageSelectCommand { get; private set; }
         public ICommand SaveCommand { get; private set; }
         public ICommand UpdateCommand { get; private set; }
         public ICommand ExitCommand { get; private set; }
         public event PropertyChangedEventHandler PropertyChanged;
 
-        int investmentAmount;
-        string name;
-        string address;
-        string ImageFullName
-        {
-            get { return imageFullName; }
-            set
-            {
-                imageFullName = value;
-
-                if (!string.IsNullOrEmpty(imageFullName))
-                {
-                    var image = new BitmapImage();
-                    image.CreateOptions = BitmapCreateOptions.None;
-                    image.BeginInit();
-                    image.UriSource = new Uri(imageFullName, UriKind.Absolute);
-                    image.EndInit();
-                    IconImage = image;
-                }
-            }
-        }
-        string imageFullName;
         BitmapImage iconImage;
-        SettingsXml xml;
 
         public ViewModel()
         {
-            xml = new SettingsXml();
-
-            if (xml.IsSuccess)
-            {
-                InvestmentAmount = xml.InvestmentAmount;
-                Name = xml.Name;
-                Address = xml.Address;
-                ImageFullName = xml.ImageFullName;
-            }
+            XPer = new XPer();
 
             SaveCommand = new DelegateCommand(() =>
             {
-                xml.InvestmentAmount = InvestmentAmount;
-                xml.Name = Name;
-                xml.Address = Address;
-                xml.ImageFullName = ImageFullName;
-                xml.Update();
-
+                XPer.Update();
                 Update();
             });
+
             ImageSelectCommand = new DelegateCommand(() =>
             {
                 var dialog = new OpenFileDialog();
@@ -120,11 +50,12 @@ namespace XPPassbook
                         }
                         var movedFileFullName = directory + Path.GetFileName(dialog.FileName);
                         File.Copy(dialog.FileName, movedFileFullName);
-                        ImageFullName = movedFileFullName;
+                        XPer.ImageFullName = movedFileFullName;
                     }
                     catch { }
                 }
             });
+
             ExitCommand = new DelegateCommand(() => System.Windows.Application.Current.MainWindow.Close() );
         }
 
