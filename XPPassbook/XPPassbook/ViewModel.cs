979632fad342655adffa79dc031d250cef615a6a
Keita <watanabe@liquidinc.asia>
2017-12-16 18:26:25 +0900

    Main画面の構成の作成

--- XPPassbook/XPPassbook/ViewModel.cs
+++ XPPassbook/XPPassbook/ViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,60 @@ namespace XPPassbook
 {
     public class ViewModel : INotifyPropertyChanged
     {
+        public int InvestmentAmount
+        {
+            get { return investmentAmount; }
+            set
+            {
+                investmentAmount = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(InvestmentAmount)));
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                address = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Address)));
+            }
+        }
+        public string ImageFullName
+        {
+            get { return imageFullName; }
+            set
+            {
+                imageFullName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ImageFullName)));
+            }
+        }
         public ICommand ExitCommand { get; private set; }
         public event PropertyChangedEventHandler PropertyChanged;
+
+        int investmentAmount;
+        string name;
+        string address;
+        string imageFullName;
+        string programDataFullName;
 
         public ViewModel()
         {
             ExitCommand = new DelegateCommand(() => Application.Current.MainWindow.Close() );
+
+            programDataFullName = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "XpJp/XP-Passbook/";
+            if (!Directory.Exists(programDataFullName))
+            {
+                Directory.CreateDirectory(programDataFullName);
+            }
         }
     }
 }
